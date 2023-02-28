964b42529303c57138c2012bffeb210f56600a64
user name <you@example.com>
2023-02-28 06:49:16 +0400

    task was sent

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿//task2
+var num = 11;
+var bolenlerinSayi = 0;
+
+for (var i = 1; i <= num; i++)
+{
+    if (num % i == 0)
+    {
+        bolenlerinSayi++;
+    }
+
+}
+if (bolenlerinSayi == 1 || bolenlerinSayi == 0)
+{
+    System.Console.WriteLine("eded ne sadedir nede murekkeb");
+        }
+else if (bolenlerinSayi > 2)
+        {
+    System.Console.WriteLine("eded murekkebdir");
+        }
+else {
+    System.Console.WriteLine("eded sadedir");
+        };
+
+
+
---
+++ task3_c/task2_c/Program.cs
@@ -0,0 +1,27 @@
+﻿var goturulenDeyer = 12;
+var bolenlerinSayi = 0;
+var sum = 0;
+for (var i = 0; i <= goturulenDeyer; i++)
+{
+    if (goturulenDeyer == 0 || goturulenDeyer == 1)
+    {
+        System.Console.WriteLine("ne tek ne cut");
+    }
+    else if (goturulenDeyer % i == 0) ;
+    {
+        bolenlerinSayi++;
+        sum += i;
+    }
+}
+if (bolenlerinSayi <= 2)
+{
+    System.Console.WriteLine(" ededimiz sade ededddir");
+            }
+else
+{
+    System.Console.WriteLine(" ededimiz sade eded deyil");
+            }
+
+System.Console.WriteLine(sum);
+
+
